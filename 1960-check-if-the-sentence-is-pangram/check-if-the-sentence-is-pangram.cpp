47706ead0003d7a75a1a440e2c7f68b9f7d4f2ed
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-07-31 01:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (35.56%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<bool>alpha(26,0) ;
+        int n = sentence.size() ;
+
+        for( int i=0 ; i<n ; i++){
+          alpha[sentence[i] -'a'] = 1 ;
+        }
+        for( int i=0 ; i<26 ; i++){
+           if( alpha[i] == 0){
+            return false ; 
+           }
+        }
+        return true ; 
+    }
+};
