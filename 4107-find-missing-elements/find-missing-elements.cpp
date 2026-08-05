99225b8e368753b620915e3b77b88774a309207c
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-08-06 02:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 32.4 MB (59.41%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,18 +1,18 @@
-// class Solution {
-// public:
-//     vector<int> findMissingElements(vector<int>& nums) {
-//         int n = nums.size() ; 
-//         sort(nums.begin() , nums.end()) ;
-//                                 // not optimal 
-//        vector<int> ans ;
-//        for( int i=0 ; i<n-1 ; i++){
-//         for(int j=nums[i]+1 ; j<nums[i+1] ; j++){
-//            ans.push_back(j) ;
-//        }
-//        }
-//        return ans ; 
-//     }
-// };
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size() ; 
+        sort(nums.begin() , nums.end()) ;
+      // not optimal nl0gn 
+       vector<int> ans ;
+       for( int i=0 ; i<n-1 ; i++){
+        for(int j=nums[i]+1 ; j<nums[i+1] ; j++){
+           ans.push_back(j) ;
+       }
+       }
+       return ans ; 
+    }
+};
 // sort(nums.begin(), nums.end());
 // vector<int> ans;
 // for (int i = 0; i < nums.size() - 1; i++) {
@@ -24,6 +24,7 @@
 // }
 
 
+// // optimal but not valid for bigger constraint O(N) , o(1) 
 // class Solution {
 // public:
 //     vector<int> findMissingElements(vector<int>& nums) {
@@ -34,7 +35,6 @@
 //           smallest = min(smallest,nums[i]) ;
 //            maxi =  max(maxi , nums[i] ) ;
 //         }
-//         // optimal but not valid for bigger constraint 
 //         vector<int> map(101,0) ;
 //         for(int i=0 ; i<n ; i++){
 //             map[nums[i]] = 1 ;
@@ -50,36 +50,36 @@
 // };
 
 
-// optimal for bigger range also 
-class Solution {
-public:
-    vector<int> findMissingElements(vector<int>& nums) {
-        int n = nums.size() ; 
+// // optimal for bigger range also o(n) , o(n) time and space 
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         int n = nums.size() ; 
        
-       int smallest = INT_MAX ,  maxi = INT_MIN ; 
-        for( int i=0 ; i<n ; i++){
-          smallest = min(smallest,nums[i]) ;
-           maxi =  max(maxi , nums[i] ) ;
-        }
-        unordered_set<int> map ;
+//        int smallest = INT_MAX ,  maxi = INT_MIN ; 
+//         for( int i=0 ; i<n ; i++){
+//           smallest = min(smallest,nums[i]) ;
+//            maxi =  max(maxi , nums[i] ) ;
+//         }
+//         unordered_set<int> map ;
 
-        for( int i=0 ; i<n ; i++){
-            map.insert(nums[i]) ;
-        }
-    vector<int> ans ; 
-    for(int i=smallest+1 ; i<maxi ; i++){
-        if(map.contains(i) == 0){
-            ans.push_back(i) ;
-        }
-    }
-    return ans ;
-    }
-};
+//         for( int i=0 ; i<n ; i++){
+//             map.insert(nums[i]) ;
+//         }
+//     vector<int> ans ; 
+//     for(int i=smallest+1 ; i<maxi ; i++){
+//         if(map.contains(i) == 0){
+//             ans.push_back(i) ;
+//         }
+//     }
+//     return ans ;
+//     }
+// };
 
-// Current complexity:
-// O(n+k)
-// Suggested complexity:
-// O(NlogN)
-// Suggestions:
-// Your approach is optimal for small ranges. For larger ranges, sorting would be more space-efficient.
+// // Current complexity:
+// // O(n+k)
+// // Suggested complexity:
+// // O(NlogN)
+// // Suggestions:
+// // Your approach is optimal for small ranges. For larger ranges, sorting would be more space-efficient.
  
