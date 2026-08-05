68d01c8826a55274724e34089fdb7474d8dfc724
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-08-06 02:40:21 +0530

    Time: 8 ms (13.35%) | Memory: 32.9 MB (43.45%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,50 @@
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         int n = nums.size() ; 
+//         sort(nums.begin() , nums.end()) ;
+//                                 // not optimal 
+//        vector<int> ans ;
+//        for( int i=0 ; i<n-1 ; i++){
+//         for(int j=nums[i]+1 ; j<nums[i+1] ; j++){
+//            ans.push_back(j) ;
+//        }
+//        }
+//        return ans ; 
+//     }
+// };
+// sort(nums.begin(), nums.end());
+// vector<int> ans;
+// for (int i = 0; i < nums.size() - 1; i++) {
+//     int current = nums[i] + 1;
+//     while (current < nums[i + 1]) {
+//         ans.push_back(current);
+//         current++;
+//     }
+// }
+
+
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size() ; 
+       
+       int smallest = INT_MAX ,  maxi = INT_MIN ; 
+        for( int i=0 ; i<n ; i++){
+          smallest = min(smallest,nums[i]) ;
+           maxi =  max(maxi , nums[i] ) ;
+        }
+        // optimal but not valid for bigger constraint 
+        vector<int> map(101,0) ;
+        for(int i=0 ; i<n ; i++){
+            map[nums[i]] = 1 ;
+        }
+        vector<int> ans ;
+        for(int i=smallest ; i<maxi ; i++){
+            if(map[i] != 1) {
+                ans.push_back(i) ;
+            }
+        }
+        return ans ; 
+    }
+};
