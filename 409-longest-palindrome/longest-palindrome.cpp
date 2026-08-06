f58cbde10e20360d81abf0a102fa634dae161f0d
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-08-07 02:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (86.67%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,43 +1,78 @@
+// class Solution {
+// public:
+//     int longestPalindrome(string s) {
+//         int n = s.size() ;
+//          vector<int> lower(26,0) ;
+//          vector<int> upper(26,0) ;
+              
+//          for(int i=0 ; i<n ; i++){
+//             if(s[i] >= 'a')
+//             {
+//             lower[s[i]-'a']++ ; 
+//            }
+//          else{
+//             upper[s[i]-'A']++ ; 
+//          }
+//          }
+
+//         int ans = 0 ;
+//         int odd = false ; 
+
+//          for(int i=0 ; i<26 ; i++){
+//             if(lower[i]%2 == 0 ) {
+//                 ans += lower[i] ;  
+//             }
+//             else{
+//                 ans += lower[i]-1 ;
+//                  odd = true ;
+//             }
+
+//             if( upper[i] % 2 == 0){
+//                 ans += upper[i] ; 
+//             }
+//             else{
+//                 ans += upper[i]-1 ; 
+//                 odd = true ;
+//             }
+//             }
+//             if(odd == true ){
+//                 ans = ans+1 ;
+//          }
+//          return ans ; 
+//     }
+// };
+
+
 class Solution {
 public:
     int longestPalindrome(string s) {
         int n = s.size() ;
-         vector<int> lower(26,0) ;
-         vector<int> upper(26,0) ;
-              
-         for(int i=0 ; i<n ; i++){
-            if(s[i] >= 'a')
-            {
-            lower[s[i]-'a']++ ; 
-           }
-         else{
-            upper[s[i]-'A']++ ; 
-         }
-         }
+        vector<int> count(52, 0) ;
+
+        for(int i=0 ; i<n ; i++){
+            if(s[i] >= 'a'){
+                count[s[i]-'a']++ ;
+            }
+            else{
+                count[26 + s[i]-'A']++ ;
+            }
+        }
 
         int ans = 0 ;
-        int odd = false ; 
+        int Odd = false ;
 
-         for(int i=0 ; i<26 ; i++){
-            if(lower[i]%2 == 0 ) {
-                ans += lower[i] ;  
+        for(int i=0 ; i<52 ; i++){
+            if(count[i] % 2 == 0){
+                ans += count[i] ;
             }
             else{
-                ans += lower[i]-1 ;
-                 odd = true ;
+                ans += count[i] - 1 ;
+                Odd = true ;
             }
-
-            if( upper[i] % 2 == 0){
-                ans += upper[i] ; 
-            }
-            else{
-                ans += upper[i]-1 ; 
-                odd = true ;
-            }
-            }
-            if(odd == true ){
-                ans = ans+1 ;
-         }
-         return ans ; 
+        }
+        if(Odd == true){
+            ans += 1 ;
+        }
+        return ans ;
     }
 };
