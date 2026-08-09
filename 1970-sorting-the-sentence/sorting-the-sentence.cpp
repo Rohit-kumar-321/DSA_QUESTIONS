f33e373aceb1358ab8b12cc8e03fc3e588a86cc8
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-08-10 01:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (21.25%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        int n = s.size() ;
-        vector<string> ans(9);
 
-        string word = "";
-
-        for(int i = 0; i <= n ; i++) {
-            if(i == n || s[i] == ' ') {
-                
-                int pos = word.back() - '0';
-                word.pop_back();
-
-                ans[pos - 1] = word;
-                word = "";
-            }
-            else {
-                word += s[i];
-            }
-        }
-
-        string result = "";
-
-        for(int i = 0; i < 9; i++) {
-            if(ans[i] != "") {
-                if(result != "") result += " ";
-                result += ans[i];
-            }
-        }
-
-        return result;
+int n = s.size() ; 
+vector<string> ans(10) ;
+string word = "";
+for( int i=0 ; i<=n ; i++){
+   if(i==n || s[i] == ' ' ){
+    int pos = word[word.size()-1]-'0' ;
+        word.pop_back() ;
+        ans[pos] = word ;
+        word = "" ;
     }
+    else{
+        word = word+s[i] ; 
+    }
+}
+string result = "" ;
+for( int i=1 ; i<=9 ; i++){
+   if( ans[i] != "" ){
+    if(result != "")
+        result = result + " " ; 
+    result += ans[i] ;  
+}
+}
+return result ;
+}
 };
