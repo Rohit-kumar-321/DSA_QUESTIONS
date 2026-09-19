0cdfe977b136742f8794f58713246f983d04a334
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-09-19 12:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (94.22%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,16 +1,32 @@
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         int n= strs.size() ;  // strs.size() = 2 in 1st example 
+//         string ans = "" ;  
+//        for( int i=0 ; i<strs[0].size() ; i++){
+//           for( int j=1 ; j<n ; j++){  // j=1 flow , j=2 flight  
+//            if(strs[0][i] != strs[j][i] ) {
+//             return ans ; 
+//            }
+//           }
+//            ans += strs[0][i] ; 
+//           }
+//           return ans ; 
+//     }
+// };
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n= strs.size() ;  // strs.size() = 2 in 1st example 
-        string ans = "" ;  
-       for( int i=0 ; i<strs[0].size() ; i++){
-          for( int j=1 ; j<n ; j++){  // j=1 flow , j=2 flight  
-           if(strs[0][i] != strs[j][i] ) {
-            return ans ; 
-           }
-          }
-           ans += strs[0][i] ; 
-          }
-          return ans ; 
+         string s = "" ; 
+        for( int i=0 ; i<strs[0].size() ; i++){
+            for(int j=1 ; j<strs.size() ; j++){
+             if( strs[0][i] != strs[j][i]) {
+              return s ; 
+             }
+            }
+             s += strs[0][i] ; 
+        }
+        return s ; 
     }
-};
+}; 
