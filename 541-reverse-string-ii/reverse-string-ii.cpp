bdbbdd398350cd4fcf00711e4f83b0a8252716c9
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-09-19 10:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (76.81%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -1,17 +1,37 @@
+// class Solution {
+// public:
+// string reverseStr(string s, int k) {
+//         int n = s.size() ;
+
+//         for ( int i=0 ; i<n ; i+= 2*k){
+//             int left = i ; 
+//             int right = min(i+k-1 , n-1) ; 
+//         while(left<right){
+//          swap(s[left],s[right]) ; 
+//          left++ , right-- ;
+//         }
+//         }
+//         return s ;
+// }
+// };
+
 class Solution {
 public:
 string reverseStr(string s, int k) {
-        int n = s.size() ;
 
-        for ( int i=0 ; i<n ; i+= 2*k){
-            int left = i ; 
-            int right = min(i+k-1 , n-1) ; 
-        while(left<right){
-         swap(s[left],s[right]) ; 
-         left++ , right-- ;
-        }
-        }
-        return s ;
+ for ( int i=0 ; i<s.size() ; i++){
+  int start=i , end = i+k-1 ; 
+
+  if( end >= s.size()){
+    end = s.size()-1 ; 
+  }
+
+  while( start <= end) {
+  swap(s[start] , s[end]) ;
+  start++ , end-- ; 
+  }
+  i = i + 2*k - 1; 
+ }
+ return s ; 
 }
-};
-
+}; 
