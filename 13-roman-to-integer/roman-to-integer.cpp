cb20c319dbeeaa639c2ef094b6cf0cbfa5607edb
Rohit kumar <rk25mmb1a01@student.nitw.ac.in>
2026-09-27 00:04:31 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (85.80%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+int num(char c ){
+       if (c== 'I') return 1 ;
+       else if (c=='V') return 5 ;
+       else if (c=='X') return 10 ; 
+       else if (c=='L') return 50 ; 
+       else if (c=='C') return 100 ; 
+       else if (c=='D') return 500 ; 
+       else return 1000 ; 
+        }
+
+    int romanToInt(string s){
+
+         int sum = 0 ; 
+         for( int i=0 ; i<s.size()-1 ; i++){ // last wala usse agle se compare na ho sakta 
+        if( num(s[i]) >= num(s[i+1]) ){
+                sum += num(s[i]) ; 
+            }
+         else{  // (s[i] < s[i+1]){
+                sum -= num(s[i]) ; 
+            } 
+         }
+         sum += num(s[s.size()-1])  ;       
+       return sum ; 
+    }
+};
